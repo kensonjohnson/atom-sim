93daaaa64c15e30c60166b6f0b20c707dd3ab91e
Kenson Johnson <kenson.johnson@outlook.com>
2023-03-21 13:41:06 -0500

    Setting up a place to store rule presets

---
+++ src/profiles.ts
@@ -0,0 +1,11 @@
+export const gravity = {
+  YELLOW_RULES: { yellow: 0.1, red: 0.0001, green: 0.2, blue: -0.0002 },
+  RED_RULES: { yellow: 0.0001, red: 0.1, green: 0.34, blue: -0.0002 },
+  GREEN_RULES: { yellow: -0.34, red: 0.17, green: 0.32, blue: -0.0002 },
+  BLUE_RULES: {
+    yellow: 0.0005,
+    red: 0.0002,
+    green: 0.0002,
+    blue: -0.001,
+  },
+};
