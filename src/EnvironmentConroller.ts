80d38fe3338bd45e36dfb7f39dadc86066f045b9
Kenson Johnson <kenson.johnson@outlook.com>
2023-03-26 23:16:43 -0500

    Converting to module

--- src/EnvironmentConroller.ts
+++ src/EnvironmentConroller.ts
@@ -1,4 +1,4 @@
-import { Atom } from "./AtomModels";
+import { AtomGroup } from "./AtomModels";
 
 export interface Rules {
   yellow: number;
@@ -7,53 +7,103 @@ export interface Rules {
   blue: number;
 }
 
-export function applyRule(
-  atomGroup1: Atom[],
-  atomGroup2: Atom[],
-  attractionForce: number,
-  width: number,
-  height: number,
-  effectRadius: number,
-  velocityBrake: number,
-  gravityWell: number
-) {
-  const centerX = width / 2;
-  const centerY = height / 2;
-  for (const baseAtom of atomGroup1) {
-    baseAtom.visualXPosition = baseAtom.xPosition;
-    baseAtom.visualYPosition = baseAtom.yPosition;
-    let forceOfX = 0;
-    let forceOfY = 0;
-    let forceOfGravityX = 0;
-    let forceOfGravityY = 0;
-    for (const opposingAtom of atomGroup2) {
-      const deltaOfX = baseAtom.xPosition - opposingAtom.xPosition;
-      const deltaOfY = baseAtom.yPosition - opposingAtom.yPosition;
-      const deltaToCenterX = baseAtom.xPosition - centerX;
-      const deltaToCenterY = baseAtom.yPosition - centerY;
-      const distance = Math.sqrt(deltaOfX * deltaOfX + deltaOfY * deltaOfY);
-      if (distance && distance < effectRadius) {
-        const force = attractionForce * (1 / distance);
-        forceOfX -= force * deltaOfX;
-        forceOfY -= force * deltaOfY;
-      }
-      // const gravityForce = gravityWell * (1 / distanceToCenter);
-      forceOfGravityX -= gravityWell * deltaToCenterX;
-      forceOfGravityY -= gravityWell * deltaToCenterY;
-    }
-    baseAtom.xVelocity =
-      (baseAtom.xVelocity + forceOfX + forceOfGravityX) * velocityBrake;
-    baseAtom.xPosition += baseAtom.xVelocity;
-    baseAtom.yVelocity =
-      (baseAtom.yVelocity + forceOfY + forceOfGravityY) * velocityBrake;
-    baseAtom.yPosition += baseAtom.yVelocity;
+// CANVAS_WIDTH,
+// CANVAS_HEIGHT,
+// ATOM_EFFECT_RADIUS,
+// VELOCITY_BRAKE,
+// GRAVITY_WELL
 
-    // This reverses an atom's direction when it hits a wall
-    if (baseAtom.xPosition < 0 || baseAtom.xPosition > width) {
-      baseAtom.xVelocity *= -1;
-    }
-    if (baseAtom.yPosition < 0 || baseAtom.yPosition > height) {
-      baseAtom.yVelocity *= -1;
+export class EnvironmentConroller {
+  private allAtomGroups: AtomGroup[];
+  width: number;
+  height: number;
+  atomEffectRadius: number;
+  velocityBrake: number;
+  gravityForce: number;
+  updatesPerSecond: number;
+  intervalTimerReference: number | null;
+  nextActionDelay: number;
+  constructor(
+    allAtomGroups: AtomGroup[],
+    width: number,
+    height: number,
+    atomEffectRadius: number,
+    velocityBrake: number,
+    gravityForce: number,
+    updatesPerSecond: number
+  ) {
+    console.log(width, height);
+    this.allAtomGroups = allAtomGroups;
+    this.width = width;
+    this.height = height;
+    this.atomEffectRadius = atomEffectRadius;
+    this.velocityBrake = velocityBrake;
+    this.gravityForce = gravityForce;
+    this.updatesPerSecond = updatesPerSecond;
+    this.intervalTimerReference = null;
+    this.nextActionDelay = 1000 / updatesPerSecond;
+  }
+
+  start() {
+    this.intervalTimerReference = setInterval(() => {
+      this.updateAtomTargetPositions();
+    }, this.nextActionDelay);
+  }
+
+  private updateAtomTargetPositions() {
+    const centerX = this.width / 2;
+    const centerY = this.height / 2;
+    for (const baseGroup of this.allAtomGroups) {
+      for (const oppositeGroup of this.allAtomGroups) {
+        const attractionForce =
+          baseGroup.rules[oppositeGroup.color as keyof Rules];
+        for (const baseAtom of baseGroup.atoms) {
+          let forceOfX = 0;
+          let forceOfY = 0;
+          let forceOfGravityX = 0;
+          let forceOfGravityY = 0;
+          for (const opposingAtom of oppositeGroup.atoms) {
+            const deltaOfX = baseAtom.xPosition - opposingAtom.xPosition;
+            const deltaOfY = baseAtom.yPosition - opposingAtom.yPosition;
+            const deltaToCenterX = baseAtom.xPosition - centerX;
+            const deltaToCenterY = baseAtom.yPosition - centerY;
+            const distance = Math.sqrt(
+              deltaOfX * deltaOfX + deltaOfY * deltaOfY
+            );
+            if (distance && distance < this.atomEffectRadius) {
+              const force = attractionForce * (1 / distance);
+              forceOfX -= force * deltaOfX;
+              forceOfY -= force * deltaOfY;
+            }
+            // const gravityForce = gravityWell * (1 / distanceToCenter);
+            forceOfGravityX -= this.gravityForce * deltaToCenterX;
+            forceOfGravityY -= this.gravityForce * deltaToCenterY;
+          }
+          baseAtom.xVelocity =
+            (baseAtom.xVelocity + forceOfX + forceOfGravityX) *
+            this.velocityBrake;
+
+          baseAtom.yVelocity =
+            (baseAtom.yVelocity + forceOfY + forceOfGravityY) *
+            this.velocityBrake;
+
+          // This reverses an atom's direction when it hits a wall
+          if (baseAtom.xPosition < 0 && baseAtom.xVelocity < 0) {
+            baseAtom.xVelocity *= -1;
+          }
+          if (baseAtom.xPosition > this.width && baseAtom.xVelocity > 0) {
+            baseAtom.xVelocity *= -1;
+          }
+          if (baseAtom.yPosition < 0 && baseAtom.xVelocity < 0) {
+            baseAtom.yVelocity *= -1;
+          }
+          if (baseAtom.yPosition > this.height && baseAtom.yVelocity > 0) {
+            baseAtom.yVelocity *= -1;
+          }
+          baseAtom.xPosition += baseAtom.xVelocity;
+          baseAtom.yPosition += baseAtom.yVelocity;
+        }
+      }
     }
   }
 }
